97d075b64ba3087990c51afdb73aa9dbf25e471c
Andrei Kazelski <andkaz@protonmail.com>
2021-05-28 13:08:45 +0200

    function sumSalaries

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sumSalaries = 0
+
+  function sumSalary(salaries) {
+    for (let key in salaries) {
+      if (typeof salaries[key] === 'number') {
+        sumSalaries = salaries[key] + salaries[key] + salaries[key]
+      }
+    }
+}
 }
