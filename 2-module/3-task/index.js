2f91660deecd1b76022b9d67ee9687cc024f8968
Andrei Kazelski <andkaz@protonmail.com>
2021-05-30 14:17:02 +0200

    added calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,27 @@
 let calculator = {
-  // ваш код
-};
+  read: function (a, b) {
+    this.a = +prompt('first number', '')
+    this.b = +prompt('second number', '')    
+   }
+   
+ }
+ 
+ 
+ calculator.read()
+ 
+ function mul () {
+   return this.a * this.b
+ }
+ 
+ calculator.mul = mul 
+ 
+ 
+ function sum () {
+   return this.a + this.b
+ }
+ 
+ calculator.sum = sum
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
