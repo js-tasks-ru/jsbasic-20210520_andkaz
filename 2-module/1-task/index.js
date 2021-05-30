60e1d06482559a0f73749dddfb161806193665ec
Andrei Kazelski <andkaz@protonmail.com>
2021-05-30 14:11:55 +0200

    2-module, 1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,6 +1,5 @@
 function sumSalary(salaries) {
-  let val = 0
-  
+  let val = 0  
 
   for (let key in salaries) {
     if (typeof salaries[key] === 'number' && !isNaN(salaries[key]) && isFinite(salaries[key]) )  {
