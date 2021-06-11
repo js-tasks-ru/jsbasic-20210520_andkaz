b46b5ee053ed419a26a3f8b25ecf68eee589fdc9
Andrei Kazelski <andkaz@protonmail.com>
2021-06-11 17:21:12 +0200

    5-module - task 1,2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,15 @@
 function toggleText() {
-  // ваш код...
+  const $text = document.querySelector('#text')
+  const $btn = document.querySelector('.toggle-text-button')
+
+  $btn.addEventListener('click', hideTxt)
+  function hideTxt() {
+    console.log('hide');
+    if ($text.hasAttribute('hidden')) {
+      return $text.removeAttribute('hidden')
+    } else {
+      return $text.hidden = 'true'
+
+    }
+  }
 }
