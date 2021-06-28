7cb0b968e10eff619ea911c6165664cf234f18e1
Andrei Kazelski <andkaz@protonmail.com>
2021-06-28 23:17:40 +0200

    carousele

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,49 @@
 function initCarousel() {
-  // ваш код...
+  const $divInner = document.querySelector('.carousel__inner')
+  const $buttonRight = document.querySelector('.carousel__arrow_right')
+  const $buttonLeft = document.querySelector('.carousel__arrow_left')
+
+
+  $buttonRight.addEventListener('click', slideRight)
+  $buttonLeft.addEventListener('click', slideLeft)
+
+  $buttonLeft.style.display = 'none' //hide left button
+
+  let count = 0
+  const step = $divInner.offsetWidth
+
+  function slideLeft() {
+    $buttonRight.style.display = 'flex' //show right button
+    console.log('click-left');
+    count -= step
+    
+    // debugger
+
+    for (; count >= 0;) {
+      if (count == 0) {
+        $buttonLeft.style.display = 'none' //hide left button
+      }
+      return $divInner.style.transform = `translateX(-${count}px)`
+    }
+  }
+
+
+  function slideRight() {
+    console.log('click-right');
+
+    count += step
+
+    $buttonLeft.style.display = 'flex' //show left button
+
+    // debugger    
+
+    for (; count < step * 4;) {
+      if (count === 2964) {
+        $buttonRight.style.display = 'none' //hide right button  
+      }
+      
+      return $divInner.style.transform = `translateX(-${count}px)`      
+    }
+   
+  }
 }
